83ea427d7522a46ee33790b334cc494299eb93ce
olgafurs <olga.m.furs@mail.ru>
2020-09-29 15:01:52 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (myArray) {
+
+  if( typeof myArray === 'undefined' || myArray.length === 0) {
+    return 0;
+  }
+
+  let min = myArray[0];
+  for( let i = 1; i < myArray.length; i++) {
+    if(myArray[i] < min){
+      min = myArray[i];
+    }    
+  }
+  return min;  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (myArray) {
+
+  if(typeof myArray === 'undefined' || myArray.length === 0) {
+    return 0;
+  }
+
+  let max = myArray[0];
+  for( let i = 1; i < myArray.length; i++) {
+    if(myArray[i] > max){
+      max = myArray[i];
+    }
+    
+  }
+
+  return max;  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (myArray) {
+
+  if(typeof myArray === 'undefined' || myArray.length === 0) {
+    return 0;
+  }
+
+  let count = 0;
+  for( let i = 0; i < myArray.length; i++) {
+    count += myArray[i]    
+  }
+
+  return count/myArray.length;  
 }
